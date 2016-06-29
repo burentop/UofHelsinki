d2e48b416da05e9847ebd2235b965565d65f24e6
Brent Perry <burentoperii@gmail.com>
2016-06-29 11:25:12 -0500

    Add print methods (Owner & RegPlate) to VehicleRegister.

--- Week8/CarRegCenter/src/VehicleRegister.java
+++ Week8/CarRegCenter/src/VehicleRegister.java
@@ -1,4 +1,7 @@
 import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class VehicleRegister {
 	
 	private HashMap<RegistrationPlate, String> owners = new HashMap<RegistrationPlate, String>();
@@ -27,5 +30,26 @@ public boolean add (RegistrationPlate plate, String owner) {
 		}
 		return false;
 	}
+	
+	public void printRegistrationPlates () {
+        for (RegistrationPlate plate : owners.keySet()) {
+            System.out.println(plate);
+        }
+    }
+    
+    public void printOwners() {
+        ArrayList<String> tempArray = new ArrayList<String>();
+        for (RegistrationPlate plate : owners.keySet()) {
+            if (!tempArray.contains(owners.get(plate))) {
+                tempArray.add(owners.get(plate));
+            }
+        }
+        
+        Iterator<String> itr = tempArray.iterator();
+        while (itr.hasNext()) {
+            Object element = itr.next();
+            System.out.println(element);
+        }
+    }
 	
 }
