e5aa35ff79a89919f067a8f26fd23a4598c98c9a
Brent Perry <burentoperii@gmail.com>
2016-06-29 15:42:41 -0500

    Add HashMap for stock, and methods to take and show stock.

--- Week8/OnlineShop/src/Storehouse.java
+++ Week8/OnlineShop/src/Storehouse.java
@@ -3,13 +3,13 @@ import java.util.HashMap;
 
 public class Storehouse {
 
-private Map<String, Integer> prices = new HashMap<String, Integer>();
-    
+	private Map<String, Integer> prices = new HashMap<String, Integer>();
+    private Map<String, Integer> stockList = new HashMap<String, Integer>();
     
     public void addProduct(String product, int price, int stock) {
         
         prices.put(product, price);
-        
+        stockList.put(product, stock);
     }
     
     public int price(String product) {
@@ -18,6 +18,24 @@ private Map<String, Integer> prices = new HashMap<String, Integer>();
         } else {
             return prices.get(product);
         }
+    }
+    
+    public int stock(String product) {
+        if (stockList.get(product) == null) return 0;
+        else return stockList.get(product);
+    }
+    
+    public boolean take(String product) {
+        if (stockList.containsKey(product)) {
+            if (stockList.get(product) > 0) {
+                int oldValue = stockList.get(product);
+                stockList.put(product, oldValue - 1);
+                return true;
+            } else {
+                return false;
+            }
+        } 
+        return false;
     }
 	
 }
