eca8236aa1dc6439874fa981fca57c81d2c0e6bf
Brent Perry <burentoperii@gmail.com>
2016-06-29 16:32:27 -0500

    Add ShoppingBasket class.

---
+++ Week8/OnlineShop/src/ShoppingBasket.java
@@ -0,0 +1,26 @@
+import java.util.Map;
+import java.util.HashMap;
+
+public class ShoppingBasket {
+
+private Map<String, Purchase> basket;
+    
+    public ShoppingBasket() {
+        basket = new HashMap<String, Purchase>();
+    }
+    
+    public void add(String product, int price) {
+        Purchase purchase = new Purchase(product, 1, price);
+        basket.put(product, purchase);
+    }
+    
+    public int price() {
+        int total = 0;
+
+        for (Purchase purchase : basket.values()) {
+            total += purchase.price();
+        }
+        return total;
+    }
+	
+}
